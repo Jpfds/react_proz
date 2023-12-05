50545bb6b1d8c19cfdc1581f9ca9e46225a4b07f
Jpfds <jallinrabbei57@gmail.com>
2023-12-05 08:15:59 -0300

    app name

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class='divh1'>
+    <h1>
+      João Pedro Ferreira da Silva
+    </h1>
     </div>
   );
 }
