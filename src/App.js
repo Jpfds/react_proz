7e788c4d31bd04cc19fd56a4a0ec8851c1ceb1db
Jpfds <jallinrabbei57@gmail.com>
2023-12-05 08:45:18 -0300

    atividade

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ function App() {
   return (
     <div class='divh1'>
     <h1>
-      João Pedro Ferreira da Silva
+     Como baixar React-Native.
     </h1>
+    <p>1-Instale o npm e o Node.js.</p>
+    <p>2-Instale o aplicativo Create React.</p>
+    <p>3-Crie um novo projeto React.</p>
+    <p>4-Vá para o diretório do projeto e inicie o servidor de desenvolvimento.</p>
     </div>
   );
 }
